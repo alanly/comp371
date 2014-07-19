37df72263e03a75b0d70200a8088918f5f031775
Alan Ly <me@alanly.ca>
2014-07-19 18:53:58 -0400

    Added 2D text helper functions from tutorial site.

---
+++ Source/Shader.h
@@ -0,0 +1,10 @@
+/**
+ * Code is courtesy of OpenGL-Tutorial.org
+ */
+
+#ifndef SHADER_H
+#define SHADER_H
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
