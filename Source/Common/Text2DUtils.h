71429669aeffa6bfb8cf20837702bb512211c336
Alan Ly <me@alanly.ca>
2014-08-02 12:02:32 -0400

    Remove unused Text2DUtils utility.

--- Source/Common/Text2DUtils.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEXT2DUTILS_H
-#define TEXT2DUTILS_H
-
-void initText2D(const char * texturePath);
-void printText2D(const char * text, int x, int y, int size);
-void cleanupText2D();
-
-#endif
