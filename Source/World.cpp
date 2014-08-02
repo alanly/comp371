158ab68a33c1b6462e84d2591cccf203565e89be
Alan Ly <me@alanly.ca>
2014-08-02 11:00:22 -0400

    Move gl shader program handling into model loop.

--- Source/World.cpp
+++ Source/World.cpp
@@ -98,17 +98,16 @@ void World::addModel(Model* nModel){
 void World::Draw()
 {
 	Renderer::BeginFrame();
-	
-	// Set Shader... In a more sofisticated application, each model could use a different shader
-	// In our case, all the models use a common shader
-	glUseProgram(Renderer::GetShaderProgramID());
-
-	// This looks for the MVP Uniform variable in the Vertex Program
-	GLuint VPMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ViewProjectonTransform"); 
 
 	// Draw models
 	for (vector<Model*>::iterator it = mModel.begin(); it < mModel.end(); ++it)
 	{
+		// Set our shader
+		glUseProgram(Renderer::GetShaderProgramID());
+
+		// This looks for the MVP Uniform variable in the Vertex Program
+		GLuint VPMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ViewProjectonTransform"); 
+
 		// Send the view projection constants to the shader
 		mat4 VP = mCamera[mCurrentCamera]->GetViewProjectionMatrix();
 		glUniformMatrix4fv(VPMatrixLocation, 1, GL_FALSE, &VP[0][0]);
