fa9955e533442385041c6531daeb04ce83f57669
Cherubyx <bloodbutterfly@gmail.com>
2014-08-10 19:40:11 -0400

    Adding another gif image

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,7 +5,6 @@
 //
 // Copyright (c) 2014 Concordia University. All rights reserved.
 //
-
 #include "Renderer.h"
 #include "World.h"
 #include "EventManager.h"
@@ -25,17 +24,17 @@ int main(int argc, char*argv[])
 	PlaneModel* p = new PlaneModel(glm::vec3(1.0f,1.0f,1.0f));
 	world.addModel(p);
 
-    // Load texture
+    //====== Load texture
     GLuint tex;
     glGenTextures(1, &tex);
 
     int width, height;
-    unsigned char* image = SOIL_load_image("C:\\Users\\Tiffany\\Google Drive\\COMP371\\Project\\Source\\Images\\alphalogo_a4arial.png", &width, &height, 0, SOIL_LOAD_RGBA);
+    unsigned char* image = SOIL_load_image(UTFConvert::GetImagePath("\\ohhellno.gif").c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
     SOIL_free_image_data(image);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
