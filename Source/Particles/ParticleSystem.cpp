7a0c0e66e8bda369a271e5885b961a7efe6427bf
Cherubyx <bloodbutterfly@gmail.com>
2014-08-10 20:24:42 -0400

    Setting up functions and variables - no functionality yet

---
+++ Source/Particles/ParticleSystem.cpp
@@ -0,0 +1,22 @@
+#include "ParticleSystem.h"
+
+ParticleSystem::ParticleSystem()
+{
+
+}
+
+ParticleSystem::~ParticleSystem()
+{
+
+}
+
+
+void ParticleSystem::Update(float dt)
+{
+
+}
+
+void ParticleSystem::Draw()
+{
+
+}
--- Source/Particles/ParticleSystem.h
+++ Source/Particles/ParticleSystem.h
@@ -6,7 +6,18 @@ using namespace std;
 class ParticleSystem : Model
 {
 public:
-        
+        ParticleSystem();
+        ~ParticleSystem();
+
+        virtual void Update(float lifespan);
+        virtual void Draw();
 private:
 
+    int numOfParticles;
+    float lifespan;
+
+    glm::vec3 pMagnitude;
+    glm::vec3 pParticles;
+
+    unsigned char * textureImage;
 };
