4a61bb6dde5b31ca15f7b611f4d2403d470a4bd3
Alan Ly <me@alanly.ca>
2014-08-12 15:30:18 -0400

    Add lighting classes.

---
+++ Source/DirectionalLight.h
@@ -0,0 +1,18 @@
+/**
+ * @author Alan Ly
+ */
+
+#pragma once
+#include "light.h"
+
+
+class DirectionalLight : public Light
+{
+public:
+	DirectionalLight();
+	DirectionalLight(glm::vec3 position);
+	~DirectionalLight(void);
+
+	void SetLightPosition(float x, float y, float z);
+};
+
